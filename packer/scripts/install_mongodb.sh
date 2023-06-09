2e3e7e58d15eb42e444221d73b657dfcc0c358cf
CHDN <den93168@gmail.com>
2023-06-10 00:32:56 +0300

    Д3№5 Подготовка образа VM&Packer.

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - 
-sudo echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - 
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
